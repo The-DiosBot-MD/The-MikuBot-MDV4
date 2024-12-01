66a6a4dabcd9d16f1317d37cf50b988b0667dcde
AdrianOficial <120221652+The-DiosBot-MD@users.noreply.github.com>
2024-12-01 15:33:31 -0300

    Add files via upload

---
+++ plugins/rpg-M_On y Off.js
@@ -0,0 +1,32 @@
+import { createHash } from 'crypto' 
+import PhoneNumber from 'awesome-phonenumber'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let pp = 'https://i.ibb.co/r7C0TNP/The-Miku-Bot-MD.jpg'
+//const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
+let user = global.db.data.users[m.sender]
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+try {
+pp = await conn.getProfilePicture(who)         //pp = await conn.getProfilePicture(who)
+} catch (e) {
+
+} finally {
+let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
+//let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+let username = conn.getName(who)
+let prem = global.prems.includes(who.split`@`[0])
+let sn = createHash('md5').update(who).digest('hex')
+let str =
+`> ⚙️ *AJUSTES* ⚙️
+${m.isGroup ? `_✅ ➤ Activado_
+_❌ ➤ Desactivado_` : `Para ver la configuración completa sólo use: *${usedPrefix}on* o *${usedPrefix}off*`}\n
+${generateCommand(commandsConfig, usedPrefix).replace(/≡/g, '𖡡')}`.trim()
+    conn.sendFile(m.chat, pp, 'pp.jpg', str, fkontak, false, { contextInfo: { mentionedJid }}) 
+  }
+}
+handler.help = ['profile [@user]']
+handler.tags = ['xp']
+handler.command = /^on|off|enable|disable?$/i
+export default handler
